d28f822af3e20305cee17afcc771492d2a379e28
Cristina Ailincai <cristina.ailincai@ft.ro>
2023-06-13 13:53:58 +0300

    renamed multiple actions to bulk actions

--- js/Components/TableBulkActions.vue
+++ js/Components/TableBulkActions.vue
@@ -1,28 +1,28 @@
 <template>
     <ButtonWithDropdown
-            v-if="props.actions"
-            ref="multiple_actions_dropdown"
-            dusk="multiple-actions-dropdown"
-            class="w-auto ml-2"
+        v-if="props.actions"
+        ref="multiple_actions_dropdown"
+        dusk="multiple-actions-dropdown"
+        class="w-auto ml-2"
     >
         <template #button>
             <Cog6ToothIcon class="w-5 text-gray-400"/>
         </template>
 
         <div
-                role="menu"
-                aria-orientation="horizontal"
-                aria-labelledby="multiple-actions-menu"
-                class="min-w-max shadow-lg"
+            role="menu"
+            aria-orientation="horizontal"
+            aria-labelledby="multiple-actions-menu"
+            class="min-w-max shadow-lg"
         >
             <button
-                    v-for="(action, key) in props.actions"
-                    :key="key"
-                    :dusk="`multiple-actions-row-${key}`"
-                    class="text-left w-full px-4 py-2 text-sm text-gray-900 dark:text-gray-300
+                v-for="(action, key) in props.actions"
+                :key="key"
+                :dusk="`multiple-actions-row-${key}`"
+                class="text-left w-full px-4 py-2 text-sm text-gray-900 dark:text-gray-300
                                 hover:bg-violet-500 hover:rounded-md hover:text-white flex flex-row"
-                    role="menuitem"
-                    @click="onActionClick(action.id)"
+                role="menuitem"
+                @click="onActionClick(action.id)"
             >
                 <component v-if="action.icon" :is="action.icon"
                            class="mr-2 h-5 w-5 text-violet-400"/>
